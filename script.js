e0b7066c11e0c3589710b7e49fe1cbf982649dda
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2021-09-11 11:26:51 -0400

    got images working

--- script.js
+++ script.js
@@ -15,22 +15,23 @@ searchBtn.addEventListener('click', function(event){
         for (let i = 0; i < data.drinks.length; i++) {
         
             var drink = data.drinks[i]
-            console.log(drink)
-        //     var element = `<div class="row">
-        //     <div class="col s12 m6">
-        //     <div class="card">
-        //     <div class="card-image card">
-        //     <img src="${drink}" width="200px" height="400px">
-        //     <span class="card-title">Grape Shimmer</span>
-        //     <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-        //     </div>
-        //     <div class="card-content">
-        //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eos nemo dolore ipsa a alias dolor! Quidem sed eius veritatis eligendi accusamus laudantium ab temporibus, dignissimos laboriosam? Rem, rerum ipsum.</p>
-        //     </div>
-        //     </div>
-        //     </div>`
+            var drinkThumbnailURL = drink.strDrinkThumb + "/preview"
+            console.log(drinkThumbnailURL)
+            var element = `<div class="row" id="simple">
+            <div class="col s12 m6">
+            <div class="card">
+            <div class="card-image card">
+            <img src="${drinkThumbnailURL}" width="200px" height="400px">
+            <span class="card-title">Grape Shimmer</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eos nemo dolore ipsa a alias dolor! Quidem sed eius veritatis eligendi accusamus laudantium ab temporibus, dignissimos laboriosam? Rem, rerum ipsum.</p>
+            </div>
+            </div>
+            </div>`
             
-        //    displayDiv.innerHTML += element 
+           displayDiv.innerHTML += element 
                 
         //          VoiceRSS.speech({
         //             key: 'b89df480eec844ff8978653fae8636f2',
