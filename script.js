4906c1ba391dee9492c9e4a2ed230175654ed241
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2021-09-11 13:15:10 -0400

    2nd click listener

--- script.js
+++ script.js
@@ -12,40 +12,59 @@ searchBtn.addEventListener('click', function(event){
         return response.json()
     })
     .then(function(data){
+        var testingSound
         for (let i = 0; i < data.drinks.length; i++) {
         
             var drink = data.drinks[i]
+            testingSound = drink
             var drinkThumbnailURL = drink.strDrinkThumb + "/preview"
             console.log(drinkThumbnailURL)
-            var element = `<div class="row" id="simple">
+            var element = `<div class="row">
             <div class="col s12 m6">
             <div class="card">
             <div class="card-image card">
             <img src="${drinkThumbnailURL}" width="200px" height="400px">
-            <span class="card-title">Grape Shimmer</span>
-            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            <span class="card-title">${drink.strDrink}</span>
+            <button type="button" data-text-to-speech="${drink.strDrink +". "+ drink.strInstructions}"
+                class="play-instructions btn-floating halfway-fab waves-effect waves-light red">Play
+            </button>
             </div>
             <div class="card-content">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eos nemo dolore ipsa a alias dolor! Quidem sed eius veritatis eligendi accusamus laudantium ab temporibus, dignissimos laboriosam? Rem, rerum ipsum.</p>
+            <p>${drink.strInstructions}</p>
             </div>
             </div>
             </div>`
             
            displayDiv.innerHTML += element 
                 
-        //          VoiceRSS.speech({
-        //             key: 'b89df480eec844ff8978653fae8636f2',
-        //             src: drink.strInstructions,
-        //             hl: 'en-us',
-        //             v: 'Mike',
-        //             r: 0, 
-        //             c: 'mp3',
-        //             f: '44khz_16bit_stereo',
-        //             ssml: false
-        //         });
+                 
                 console.log(data.drinks[0].strDrinkThumb + "/preview")
                 console.log(data.drinks[i])
          }
-      
+         //search for drink, look over drinks, build html, 
+        //  var drinkNameInstructions = testingSound.strDrink +". "+  testingSound.strInstructions
+        //  VoiceRSS.speech({
+        //     key: 'b89df480eec844ff8978653fae8636f2',
+        //     src: drinkNameInstructions,
+        //     hl: 'en-us',
+        //     v: 'Mike',
+        //     r: 0, 
+        //     c: 'mp3',
+        //     f: '44khz_16bit_stereo',
+        //     ssml: false
+        // });
    })
 })
+//event listener on drink display div, "simple"
+displayDiv.addEventListener('click', function(event){
+    console.log(event.target)
+    //look for class of play instructions on the click event
+    if (event.target.classList.contains('play-instructions')) {
+        console.log('hello world')
+    }
+})
+
+
+//if play instructions are presetnt play button clicked 
+//get text to speech from data attribute on the html button
+//send text to speech request (play instructions)
