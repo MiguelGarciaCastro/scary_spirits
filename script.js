6016ff576ce5492f69baf2123b95427bf7b326f7
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2021-09-11 10:46:45 -0400

    adding images

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var searchInput = document.getElementById('search')
 var searchBtn = document.getElementById('search-button')
 var displayDiv = document.getElementById('simple')
 
+
 searchBtn.addEventListener('click', function(event){
     event.preventDefault()
     var searchTerm = searchInput.value
@@ -11,27 +12,39 @@ searchBtn.addEventListener('click', function(event){
         return response.json()
     })
     .then(function(data){
-        console.log(data)
-         var firstDrink = data.drinks[0]
-         VoiceRSS.speech({
-            key: 'b89df480eec844ff8978653fae8636f2',
-            src: firstDrink.strInstructions,
-            hl: 'en-us',
-            v: 'Mike',
-            r: 0, 
-            c: 'mp3',
-            f: '44khz_16bit_stereo',
-            ssml: false
-        });
-    
-    })
-})
-
-
-
-// function(){
-//     for (let I = 0; I < array.length; I++) {
-    
+        for (let i = 0; i < data.drinks.length; i++) {
         
-//     }
-// }
+            var drink = data.drinks[i]
+            console.log(drink)
+        //     var element = `<div class="row">
+        //     <div class="col s12 m6">
+        //     <div class="card">
+        //     <div class="card-image card">
+        //     <img src="${drink}" width="200px" height="400px">
+        //     <span class="card-title">Grape Shimmer</span>
+        //     <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        //     </div>
+        //     <div class="card-content">
+        //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eos nemo dolore ipsa a alias dolor! Quidem sed eius veritatis eligendi accusamus laudantium ab temporibus, dignissimos laboriosam? Rem, rerum ipsum.</p>
+        //     </div>
+        //     </div>
+        //     </div>`
+            
+        //    displayDiv.innerHTML += element 
+                
+        //          VoiceRSS.speech({
+        //             key: 'b89df480eec844ff8978653fae8636f2',
+        //             src: drink.strInstructions,
+        //             hl: 'en-us',
+        //             v: 'Mike',
+        //             r: 0, 
+        //             c: 'mp3',
+        //             f: '44khz_16bit_stereo',
+        //             ssml: false
+        //         });
+                console.log(data.drinks[0].strDrinkThumb + "/preview")
+                console.log(data.drinks[i])
+         }
+      
+   })
+})
